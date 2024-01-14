170778df9e7b5768e75673da669d751ccfe7acef
Joe Mansour <joemansour2003@gmail.com>
2024-01-13 21:47:20 -0500

    Work on it FATIMA

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Marketplace from "./pages/Marketplace";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-function App( {Component, pageProps} ) {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+function App({ Component, pageProps }) {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Marketplace />}>
+                    {/* <Route index element={<Marketplace />} /> */}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
