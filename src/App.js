3b5eb6b0a55095483dd02014c5f2c3331888b60d
jackdkdf <jack@rockyroost.com>
2024-01-13 22:52:57 -0500

    Added bootstrap imports

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/navbar';
 import Cart from './pages/Cart';
 import Property from './pages/Marketplace';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App( {Component, pageProps} ) {
   return (
