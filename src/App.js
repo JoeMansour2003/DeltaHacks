dee64ad2477cdfe2bc66054935462fdf1e657fa1
Joe Mansour <joemansour2003@gmail.com>
2024-01-13 23:28:52 -0500

    image update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar';
-import Cart from './pages/Cart';
-import Marketplace from './pages/Marketplace';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Cart from "./pages/Cart";
+import Marketplace from "./pages/Marketplace";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-function App( {Component, pageProps} ) {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Marketplace />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </Router>
-  );
+function App({ Component, pageProps }) {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Marketplace />} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
